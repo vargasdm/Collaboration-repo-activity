a644933a7f2a00bce201f3b7779df6eea24078ac
Seabass <beattycharles@yahoo.com>
2022-11-26 21:25:42 -0500

    api retrieval stuck on last input

--- assets/js/recipiescript.js
+++ assets/js/recipiescript.js
@@ -1,5 +1,5 @@
 var whatCuisine = "american";
-var tester = document.getElementById("#tester");
+var tester = document.querySelector("#tester");
 //localStorage.getItem("choice");
 
 function getRecipie() {
@@ -15,27 +15,31 @@ function getRecipie() {
     `https://edamam-recipe-search.p.rapidapi.com/search?q=${whatCuisine}`, options)
     .then((response) => response.json())
     .then((response) => console.log(response))
-    .then (function (hits) {
-     // var foodImg = hits[0].recipe.image;
-       foodLink = hits[0].recipe.shareAs;
-       foodIng = hits[0].recipe.ingredientLines;
-       foodImg = hits[0].recipe
-      console.log(foodImg, foodLink, foodIng);
+    .then (function (data) {
+    temp="";
+    for(i=0;i<4;i++){
+      foodName = data.hits=[0].recipe.label;
+      foodLink = data.hits=[0].recipe.shareAs;
+      //foodIng = data.hits=[0].recipe.ingredientLines;
+      foodImg = data.hits=[0].recipe.image;
+    }
+      populate()
     })
-    .catch((err) => console.error(err))
-    populate()
-};
- function populate(){
-  var recipeName = document.querySelectorAll('card-header-title')
-  var foodImgage = document.querySelectorAll('image')
-  var recipeInfo = document.querySelectorAll('content')
+    };
+    function populate(){
+      var recipeName = document.querySelector('.card-header-title')
+      var foodImage = document.querySelectorAll('.image')
+      var recipeInfo = document.querySelectorAll('content')
+      
+      recipeName.innerHTML = foodName;
+      foodImage.replace(img) = foodImg;
+    };
+    tester.addEventListener('click', getRecipie);
+    //getRecipie();
+    //mexican, italian,  chinese, american
+    //${chinese}
+    // for loop (in html make ul)
+    //create li element & li.textContent = data.hits.recipe.ingredientLines[i]
+    //append li to ul.
 
-
- };
-//getRecipie();
-tester.addEventListener('click', getRecipie);
-//mexican, italian,  chinese, american
-//${chinese}
-// for loop (in html make ul)
-//create li element & li.textContent = data.hits.recipe.ingredientLines[i]
-//append li to ul.
+    
