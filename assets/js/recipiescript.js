fac60817e13f358f39538f1c2a12bca2e040dc17
beattycharles <113803621+beattycharles@users.noreply.github.com>
2022-11-27 15:10:50 -0500

    working pulls add elements

--- assets/js/recipiescript.js
+++ assets/js/recipiescript.js
@@ -1,5 +1,6 @@
-var whatCuisine = "american";
+var whatCuisine =  JSON.parse(localStorage.getItem("cuisine-name")); //"american";
 var tester = document.querySelector("#tester");
+var newRecipie = document.getElementById('recipieList')
 //localStorage.getItem("choice");
 
 function getRecipie() {
@@ -14,32 +15,40 @@ function getRecipie() {
   fetch(
     `https://edamam-recipe-search.p.rapidapi.com/search?q=${whatCuisine}`, options)
     .then((response) => response.json())
-    .then((response) => console.log(response))
-    .then (function (data) {
-    temp="";
-    for(i=0;i<4;i++){
-      foodName = data.hits[0].recipe.label;
-      foodLink = data.hits=[0].recipe.shareAs;
-      //foodIng = data.hits=[0].recipe.ingredientLines;
-      foodImg = data.hits=[0].recipe.image;
-    }
-      populate()
-    })
-    };
+    .then((response) => {
+    recipeInfo = response
+    console.log(recipeInfo);
+      console.log(response);
+ var foodName = recipeInfo.hits[0].recipe.label;
+ var foodLink = recipeInfo.hits[0].recipe.shareAs;
+ var  foodImg = recipeInfo.hits[0].recipe.image;
+ console.log(foodName);
+ console.log(foodLink);
+ console.log(foodImg);
+ } )}
+    ;
+    
     function populate(){
-      var recipeName = document.getElementById('#recipe-1')
-      var foodImage = document.getElementById('#recipe-1')
-      var recipeInfo = document.getElementById('#recipe-1')
+      for (var i = 0; i < 5; i++) {
+        var listInfo = document.createElement("li");
+        newRecipie.classList.add("m-3", "is-size-4", "has-text-weight-medium")
+        newRecipie.style.display = "center";
+        newRecipie.textContent = foodName[i];
+        listInfo.appendChild(newRecipie)
+      // var recipeName = document.getElementById('#recipe-1')
+      // var foodImage = document.getElementById('#recipe-1')
+      // var foodInfo = document.getElementById('#recipe-1')
       
       recipeName.innerHTML = foodName;
       foodImage.replace(img) = foodImg;
-    };
-    tester.addEventListener('click', getRecipie);
+    }};
+    //tester.addEventListener('click', getRecipie);
     //getRecipie();
     //mexican, italian,  chinese, american
     //${chinese}
     // for loop (in html make ul)
     //create li element & li.textContent = data.hits.recipe.ingredientLines[i]
     //append li to ul.
+    //for(i=0;i<4;i++){}
 
     
